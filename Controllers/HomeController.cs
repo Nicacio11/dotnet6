e24126f5fb087fd877d047393127817141a65525
Vitor Nicacio <vitornicacio10@hotmail.com>
2022-07-15 11:15:25 -0300

    feat: add home controller

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Blog.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("health-check")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
