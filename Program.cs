ce7647c73af28a0fcc4c090c1c693d25f2e0fab8
Vitor Nicacio <vitornicacio10@hotmail.com>
2022-07-18 15:02:36 -0300

    feat: add token configuration

--- Program.cs
+++ Program.cs
@@ -1,7 +1,29 @@
+using System.Text;
+using Blog;
 using Blog.Data;
 using Blog.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var key = Encoding.ASCII.GetBytes(Configuration.JwtTokenKey);
+
+// configurando autorização com bearer
+builder.Services.AddAuthentication(x => 
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x => 
+{
+    x.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
 
 builder.Services.AddControllers().ConfigureApiBehaviorOptions(options => 
 {
@@ -14,6 +36,11 @@ builder.Services.AddTransient<TokenService>();
 // builder.Services.AddScoped<TokenService>(); // Vai durar por transação, dentro de um escopo de método
 // builder.Services.AddSingleton<TokenService>(); // vai vai durar para sempre uma instancia para toda aplicação
 var app = builder.Build();
+
+
+//configurando autenticação e autorização
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
