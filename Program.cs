70ffd3f3672a38dd2fdcfb6d39d85aba8f6f7495
Vitor Nicacio <vitornicacio10@hotmail.com>
2022-08-08 12:15:20 -0300

    feat: :sparkles: add ignore cycles reference

--- Program.cs
+++ Program.cs
@@ -1,13 +1,25 @@
+using System.Text.Json.Serialization;
 using Blog.Configurations;
+using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.AddConfiguration();
 builder.AddAuthentication();
 builder.Services.AddServices();
 
 
-builder.Services.AddControllers().ConfigureApiBehaviorOptions(options => 
+builder.Services.AddControllers()
+.ConfigureApiBehaviorOptions(options => 
 {
     options.SuppressModelStateInvalidFilter = true;
+})
+.AddJsonOptions(x => 
+{
+    // configuration to ignore cycles reference
+    x.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+
+    //ignore null object
+    x.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingDefault;
 });
 
 var app = builder.Build();
